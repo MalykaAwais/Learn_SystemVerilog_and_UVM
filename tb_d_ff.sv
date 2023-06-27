eda23e73f20fdf221e635d5ad1db7d437aec6291
MalykaAwais <79397101+MalykaAwais@users.noreply.github.com>
2023-06-27 08:33:06 +0500

    Update tb_d_ff.sv

--- tb_d_ff.sv
+++ tb_d_ff.sv
@@ -1,28 +1,33 @@
+// Code your testbench here
+// or browse Examples
 // File : tb_top.sv 
 module tb_d_ff (); 
   reg clk; 
-  reg resetn; 
-  reg d; 
+  reg rstn; 
+  reg data; 
   wire q; 
 // Instantiate the design 
   
   d_ff d_ff0 ( .clk (clk), .rstn (rstn), .data (data), .q (q)); 
 // Create a clock 
   
-  always #10 clk <= clk; 
+
 initial 
   begin 
-  $dumpfile("dump.vcd");
-  $dumpvars(1);
-    resetn <= 0; 
-    d <= 0; 
-  end
+  //$dumpfile("dump.vcd");
+  //$dumpvars(1);
+    clk =1;
+    rstn = 0; 
+    data = 0;
+end 
+always #10 clk = ~clk;
   always
-    begin
-    #10 resetn <= 1; 
-    #5 d <= 1; 
-    #8 d <= 0; 
-    #2 d <= 1; 
-    #10 d <= 0; 
+begin 
+     
+    #100 rstn = 1; 
+    #50 data = 1; 
+    #80 data = 0; 
+    #20 data = 1; 
+    #100 data = 0; 
     end 
 endmodule 
