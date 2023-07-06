6ad8e9a5edfe67d226563e2365d242943ccaf0be
SajidGulKhawaja <70131691+SajidGulKhawaja@users.noreply.github.com>
2023-07-06 13:18:19 +0500

    tb_temp_sensor.sv

--- temperature_sensor/tb_temp_sensor.sv
+++ temperature_sensor/tb_temp_sensor.sv
@@ -19,10 +19,12 @@ initial
     rstn = 0; 
 end 
 always #10 clk = ~clk;
-always  #100 rstn = 1; 
-always @ *
+initial #100 rstn = 1;
+//always  #100 rstn = 1; //Why use always block for this?
+  
+always @ *    //Why have you used always@* here?
 begin 
-#100 rstn = 1; 
+#100 rstn = 1; //why are you waiting for 100 secs before updation everytime? 
 
 
 sensor1.sensorID = "2A";
@@ -34,7 +36,7 @@ else
 $error("Assertion Failed!, Expected: 1");
 end 
 
-
 temp_sensor uut1(.clk(clk), .rstn(rstn), .sensorID(sensor1.sensorID), .temp(sensor1.temp), .output_state(sensor1.output_state));
 temp_sensor uut2(.clk(clk), .rstn(rstn), .sensorID(sensor2.sensorID), .temp(sensor2.temp), .output_state(sensor2.output_state));
+
 endmodule
